3b9caccbeb2557bac3e77c31d5cd174d3d61ebc3
Jeremy Warren <jwarren@bp-3.com>
2018-10-22 10:49:03 -0500

    Add initial PurchaseRequest implementation

---
+++ src/main/java/org/folio/rest/delegate/PrSendRequestNotificationDelegate.java
@@ -0,0 +1,20 @@
+package org.folio.rest.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrSendRequestNotificationDelegate extends AbstractRuntimeDelegate {
+
+  @Override
+  public void execute(DelegateExecution execution) throws Exception {
+    log.info("Executing Send Request Notification Delegate");
+
+    String orderId = execution.getVariable("orderId").toString();
+    String bookTitle = execution.getVariable("bookTitle").toString();
+    String bookId = execution.getVariable("bookId").toString();
+
+    log.info("Order {} has arrived, book {} with id {} has been received", orderId, bookTitle, bookId);
+  }
+
+}
