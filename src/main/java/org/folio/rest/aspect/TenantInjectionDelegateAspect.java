946d3318fca4ac2fe6511ecbcd32f7f9c53c2695
William Welling <wwelling@library.tamu.edu>
2018-11-06 12:28:57 -0600

    ensure task and execution listeners are tenant isolated

--- src/main/java/org/folio/rest/aspect/TenantInjectionDelegateAspect.java
+++ src/main/java/org/folio/rest/aspect/TenantInjectionDelegateAspect.java
@@ -3,6 +3,7 @@ package org.folio.rest.aspect;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.folio.rest.tenant.config.TenantConfig;
 import org.folio.rest.tenant.exception.NoTenantException;
 import org.folio.rest.tenant.storage.ThreadLocalStorage;
@@ -18,9 +19,21 @@ public class TenantInjectionDelegateAspect {
 
   @Before("execution(* org.camunda.bpm.engine.delegate.JavaDelegate.execute (org.camunda.bpm.engine.delegate.DelegateExecution)) && args(execution)")
   public void beforeDelegateExecution(DelegateExecution execution) {
-    String tenant = execution.getTenantId();
+    setTenant(execution.getTenantId());
+  }
+
+  @Before("execution(* org.camunda.bpm.engine.delegate.ExecutionListener.notify (org.camunda.bpm.engine.delegate.DelegateExecution)) && args(execution)")
+  public void beforeExecutionListenerNotify(DelegateExecution execution) {
+    setTenant(execution.getTenantId());
+  }
+
+  @Before("execution(* org.camunda.bpm.engine.delegate.TaskListener.notify (org.camunda.bpm.engine.delegate.DelegateTask)) && args(task)")
+  public void beforeTaskListenerNotify(DelegateTask task) {
+    setTenant(task.getTenantId());
+  }
+
+  private void setTenant(String tenant) {
     if (tenant == null) {
-      // NOTE: force tenant is also used to enforce HTTP requests to contain tenant header
       if (tenantConfig.isForceTenant()) {
         throw new NoTenantException("No tenant found in thread safe tenant storage!");
       }
