2ecfe494fed9f2ebc5a312eb6d7c206a09a467af
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2019-08-01 13:37:29 -0500

    Added endpoint and permissions

--- src/main/java/org/folio/rest/controller/WorkflowController.java
+++ src/main/java/org/folio/rest/controller/WorkflowController.java
@@ -2,25 +2,25 @@ package org.folio.rest.controller;
 
 import org.folio.rest.annotation.TokenHeader;
 import org.folio.rest.exception.UnableToActivateWorkflowException;
+import org.folio.rest.exception.UnableToDeactivateWorkflowException;
 import org.folio.rest.exception.WorkflowAlreadyActiveException;
 import org.folio.rest.model.Workflow;
 import org.folio.rest.service.CamundaApiService;
 import org.folio.rest.tenant.annotation.TenantHeader;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/workflows")
+@RequestMapping("/workflow")
 public class WorkflowController {
 
   @Autowired
   private CamundaApiService camundaApiService;
 
-  @PostMapping("/{id}/activate")
+  @PostMapping("/activate")
   public Workflow activateWorkflow(
     @TenantHeader String tenant,
     @TokenHeader String token,
@@ -28,5 +28,15 @@ public class WorkflowController {
   ) throws WorkflowAlreadyActiveException, UnableToActivateWorkflowException {
 
     return camundaApiService.deployWorkflow(workflow, tenant, token);
+  }
+
+  @PostMapping("/deactivate")
+  public Workflow deactivateWorkflow(
+    @TenantHeader String tenant,
+    @TokenHeader String token,
+      @RequestBody Workflow workflow
+  ) throws UnableToDeactivateWorkflowException {
+
+    return camundaApiService.undeployWorkflow(workflow, tenant, token);
   }
 }
