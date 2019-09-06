f4dd31dd4b39fd6cea3da83b6d7bb0a0a7ea9d44
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2019-09-06 10:39:47 -0500

    Using js utils

--- src/main/resources/scripts/engine.js
+++ src/main/resources/scripts/engine.js
@@ -1,6 +1,6 @@
 var %s = function(inArgs) {
-  var ScriptEngineUtilityClass = Java.type("org.folio.rest.utility.ScriptEngineUtility");
-  var scriptEngineUtility = new ScriptEngineUtilityClass();
+  // var ScriptEngineUtilityClass = Java.type("org.folio.rest.utility.ScriptEngineUtility");
+  // var scriptEngineUtility = new ScriptEngineUtilityClass();
   var args = JSON.parse(inArgs);
   var returnObj = {};
   %s
