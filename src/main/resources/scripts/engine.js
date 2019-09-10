774c5deec7fe4777f7a520f500f4e7f030550e95
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2019-09-10 16:38:42 -0500

    Added UUID class to JS context

--- src/main/resources/scripts/engine.js
+++ src/main/resources/scripts/engine.js
@@ -1,4 +1,5 @@
 var %s = function(inArgs) {
+  var UUID = Java.type("java.util.UUID");
   // var ScriptEngineUtilityClass = Java.type("org.folio.rest.utility.ScriptEngineUtility");
   // var scriptEngineUtility = new ScriptEngineUtilityClass();
   var args = JSON.parse(inArgs);
