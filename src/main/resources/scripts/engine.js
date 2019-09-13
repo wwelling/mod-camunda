e302a92199a77f599f431a2d726b0f08dd727b74
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2019-09-13 16:06:22 -0500

    Moved UUID from engine to utils

--- src/main/resources/scripts/engine.js
+++ src/main/resources/scripts/engine.js
@@ -1,5 +1,4 @@
 var %s = function(inArgs) {
-  var UUID = Java.type("java.util.UUID");
   // var ScriptEngineUtilityClass = Java.type("org.folio.rest.utility.ScriptEngineUtility");
   // var scriptEngineUtility = new ScriptEngineUtilityClass();
   var args = JSON.parse(inArgs);
