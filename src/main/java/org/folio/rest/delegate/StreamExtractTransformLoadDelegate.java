f8878aeee72e7a6056e76684b2608ce7034aaf2d
William Welling <wwelling@library.tamu.edu>
2020-01-20 18:22:11 -0600

    serialize expressions for stream extract transform load delegate

--- src/main/java/org/folio/rest/delegate/StreamExtractTransformLoadDelegate.java
+++ src/main/java/org/folio/rest/delegate/StreamExtractTransformLoadDelegate.java
@@ -1,13 +1,20 @@
 package org.folio.rest.delegate;
 
+import java.util.List;
+import java.util.Map;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.Expression;
 import org.camunda.bpm.model.bpmn.instance.FlowElement;
+import org.folio.rest.workflow.model.Request;
+import org.folio.rest.workflow.model.Stream;
 import org.folio.rest.workflow.model.StreamingExtractTransformLoadTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.core.type.TypeReference;
 
 @Service
 @Scope("prototype")
@@ -31,6 +38,20 @@ public class StreamExtractTransformLoadDelegate extends AbstractWorkflowIODelega
     String delegateName = bpmnModelElement.getName();
 
     logger.info("{} started", delegateName);
+
+    List<Stream> streams = objectMapper.readValue(this.streams.getValue(execution).toString(),
+        new TypeReference<List<Stream>>() {
+        });
+
+    List<Process> processes = objectMapper.readValue(this.processes.getValue(execution).toString(),
+        new TypeReference<List<Process>>() {
+        });
+
+    List<Request> requests = objectMapper.readValue(this.requests.getValue(execution).toString(),
+        new TypeReference<List<Request>>() {
+        });
+
+    Map<String, Object> input = getInputs(execution);
 
   }
 
