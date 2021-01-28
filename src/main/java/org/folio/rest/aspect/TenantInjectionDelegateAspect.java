5973be91a3d569012a851d01b5a80e9c9acda04e
William Welling <wwelling@library.tamu.edu>
2021-01-28 13:26:27 -0600

    upgrade core imports

--- src/main/java/org/folio/rest/aspect/TenantInjectionDelegateAspect.java
+++ src/main/java/org/folio/rest/aspect/TenantInjectionDelegateAspect.java
@@ -4,8 +4,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.DelegateTask;
-import org.folio.spring.tenant.config.TenantConfig;
 import org.folio.spring.tenant.exception.NoTenantException;
+import org.folio.spring.tenant.properties.TenantProperties;
 import org.folio.spring.tenant.storage.ThreadLocalStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 public class TenantInjectionDelegateAspect {
 
   @Autowired
-  private TenantConfig tenantConfig;
+  private TenantProperties tenantProperties;
 
   @Before("execution(* org.camunda.bpm.engine.delegate.JavaDelegate.execute (org.camunda.bpm.engine.delegate.DelegateExecution)) && args(execution)")
   public void beforeDelegateExecution(DelegateExecution execution) {
@@ -34,10 +34,10 @@ public class TenantInjectionDelegateAspect {
 
   private void setTenant(String tenant) {
     if (tenant == null) {
-      if (tenantConfig.isForceTenant()) {
+      if (tenantProperties.isForceTenant()) {
         throw new NoTenantException("No tenant found in thread safe tenant storage!");
       }
-      tenant = tenantConfig.getDefaultTenant();
+      tenant = tenantProperties.getDefaultTenant();
     }
     ThreadLocalStorage.setTenant(tenant);
   }
--- src/main/java/org/folio/rest/controller/advice/WorkflowControllerAdvice.java
+++ src/main/java/org/folio/rest/controller/advice/WorkflowControllerAdvice.java
@@ -2,7 +2,7 @@ package org.folio.rest.controller.advice;
 
 import org.folio.rest.exception.WorkflowAlreadyActiveException;
 import org.folio.rest.exception.WorkflowAlreadyDeactivatedException;
-import org.folio.spring.model.response.Errors;
+import org.folio.spring.model.response.ResponseErrors;
 import org.folio.spring.utility.ErrorUtility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,14 +18,14 @@ public class WorkflowControllerAdvice {
 
   @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
   @ExceptionHandler(WorkflowAlreadyActiveException.class)
-  public Errors handleWorkflowAlreadyActiveException(WorkflowAlreadyActiveException exception) {
+  public ResponseErrors handleWorkflowAlreadyActiveException(WorkflowAlreadyActiveException exception) {
     logger.debug(exception.getMessage(), exception);
     return ErrorUtility.buildError(exception, HttpStatus.NOT_ACCEPTABLE);
   }
 
   @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
   @ExceptionHandler(WorkflowAlreadyDeactivatedException.class)
-  public Errors handleWorkflowAlreadyDeactivatedException(WorkflowAlreadyDeactivatedException exception) {
+  public ResponseErrors handleWorkflowAlreadyDeactivatedException(WorkflowAlreadyDeactivatedException exception) {
     logger.debug(exception.getMessage(), exception);
     return ErrorUtility.buildError(exception, HttpStatus.NOT_ACCEPTABLE);
   }
