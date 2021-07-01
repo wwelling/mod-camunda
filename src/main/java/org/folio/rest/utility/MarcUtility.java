7581aa0582e1d6d2fbeb21d1c25003ac8f864bb6
William Welling <wwelling@library.tamu.edu>
2021-07-01 10:01:31 -0500

    marc utility methods

--- src/main/java/org/folio/rest/utility/MarcUtility.java
+++ src/main/java/org/folio/rest/utility/MarcUtility.java
@@ -8,6 +8,7 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -17,14 +18,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import org.marc4j.MarcException;
 import org.marc4j.MarcJsonReader;
 import org.marc4j.MarcJsonWriter;
 import org.marc4j.MarcStreamReader;
+import org.marc4j.MarcStreamWriter;
+import org.marc4j.MarcWriter;
+import org.marc4j.marc.ControlField;
+import org.marc4j.marc.DataField;
+import org.marc4j.marc.MarcFactory;
 import org.marc4j.marc.Record;
 import org.marc4j.marc.Subfield;
 import org.marc4j.marc.VariableField;
@@ -67,22 +75,81 @@ public class MarcUtility {
     }
 
     return records.stream()
-      .map(this::recordToJson)
+      .map(this::recordToMarcJson)
       .collect(Collectors.toList());
   }
 
-  /**
-   * Convert a String representing marc binary into a JSON string.
-   *
-   * @param rawMarc The marcBinary String to convert into json.
-   *
-   * @return A String containing the encoded JSON marc data.
-   * @throws IOException
-   */
-  public String rawMarcToJson(String rawMarc) {
+  public String addFieldToMarcJson(String marcJson, String fieldJson) throws JsonMappingException, JsonProcessingException {
+    JsonNode fieldNode = mapper.readTree(fieldJson);
+    MarcFactory factory = MarcFactory.newInstance();
+    Optional<Record> record = marcJsonToRecord(marcJson);
+    if (record.isPresent()) {
+      String tag = fieldNode.get("tag").asText();
+
+      DataField field = factory.newDataField();
+      field.setTag(tag);
+
+      if (fieldNode.has("indicator1")) {
+        char indicator1 = fieldNode.get("indicator1").asText().charAt(0);
+        field.setIndicator1(indicator1);
+      }
+      if (fieldNode.has("indicator2")) {
+        char indicator2 = fieldNode.get("indicator2").asText().charAt(0);
+        field.setIndicator2(indicator2);
+      }
+
+      ArrayNode subfields = (ArrayNode) fieldNode.get("subfields");
+
+      subfields.forEach(subfieldNode -> {
+        char code = subfieldNode.get("code").asText().charAt(0);
+        String data = subfieldNode.get("data").asText();
+        Subfield subfield = factory.newSubfield();
+        subfield.setCode(code);
+        subfield.setData(data);
+        field.addSubfield(subfield);
+      });
+
+      record.get().addVariableField(field);
+
+      recalculateLeader(record.get());
+
+      return recordToMarcJson(record.get());
+    }
+
+    return marcJson;
+  }
+
+  public String updateControlNumberField(String marcJson, String data) {
+    Optional<Record> record = marcJsonToRecord(marcJson);
+    if (record.isPresent()) {
+      if (Objects.nonNull(record.get().getControlNumberField())) {
+        record.get().getControlNumberField().setData(data);
+      }
+      else {
+        ControlField controlField = MarcFactory.newInstance().newControlField("001");
+        controlField.setData(data);
+        record.get().addVariableField(controlField);
+      }
+      recalculateLeader(record.get());
+
+      return recordToMarcJson(record.get());
+    }
+
+    return marcJson;
+  }
+
+  public String marcJsonToRawMarc(String marcJson) {
+    Optional<Record> record = marcJsonToRecord(marcJson);
+    if (record.isPresent()) {
+      return recordToRawMarc(record.get());
+    }
+    return "";
+  }
+
+  public String rawMarcToMarcJson(String rawMarc) {
     Optional<Record> record = rawMarcToRecord(rawMarc);
     if (record.isPresent()) {
-      return recordToJson(record.get());
+      return recordToMarcJson(record.get());
     }
     return "{}";
   }
@@ -135,7 +202,7 @@ public class MarcUtility {
     return Optional.empty();
   }
 
-  private String recordToJson(Record record) {
+  private String recordToMarcJson(Record record) {
     try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
       final MarcJsonWriter writer = new MarcJsonWriter(out);
       writer.write(record);
@@ -148,6 +215,19 @@ public class MarcUtility {
     return "{}";
   }
 
+  private String recordToRawMarc(Record record) {
+    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+      final MarcStreamWriter writer = new MarcStreamWriter(out);
+      writer.write(record);
+      writer.close();
+      return out.toString();
+    } catch (IOException e) {
+      // TODO: do something in case of exception
+      System.out.println(e.getMessage());
+    }
+    return "";
+  }
+
   private String getRecordFields(Record record, String[] tags) {
     List<VariableField> fields = record.getVariableFields(tags);
     try {
@@ -157,6 +237,18 @@ public class MarcUtility {
       System.out.println(e.getMessage());
     }
     return "[]";
+  }
+
+  private void recalculateLeader(Record record) {
+    try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+      MarcWriter streamWriter = new MarcStreamWriter(os, DEFAULT_CHARSET.name());
+      // use stream writer to recalculate leader
+      streamWriter.write(record);
+      streamWriter.close();
+    } catch (IOException e) {
+      // TODO: do something in case of exception
+      System.out.println(e.getMessage());
+    }
   }
 
 }
