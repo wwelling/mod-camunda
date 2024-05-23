5bdd2386a7660c4de69366d162cc4843b812a732
Kevin Day <35114839+kaladay@users.noreply.github.com>
2024-05-23 14:20:19 -0500

    Update src/main/resources/scripts/engine.py
    Co-authored-by: William Welling <144841721+wwelling@users.noreply.github.com>

--- src/main/resources/scripts/engine.py
+++ src/main/resources/scripts/engine.py
@@ -2,7 +2,7 @@ import json
 import org.folio.rest.camunda.utility.ScriptEngineUtility;
 
 def %s(inArgs):
-  scriptEngineUtility = org.folio.rest.utility.ScriptEngineUtility();
+  scriptEngineUtility = org.folio.rest.camunda.utility.ScriptEngineUtility();
   args = scriptEngineUtility.decodeJson(inArgs);
   returnObj = scriptEngineUtility.createJson();
   %s
