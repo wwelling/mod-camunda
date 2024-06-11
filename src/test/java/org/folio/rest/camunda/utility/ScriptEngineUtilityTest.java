0b480c00245dd368088db8f946ad46874bf4da63
William Welling <wwelling@tamu.edu>
2024-06-11 13:38:14 -0500

    Update ScriptEngineUtilityTest.java

--- src/test/java/org/folio/rest/camunda/utility/ScriptEngineUtilityTest.java
+++ src/test/java/org/folio/rest/camunda/utility/ScriptEngineUtilityTest.java
@@ -34,19 +34,18 @@ public class ScriptEngineUtilityTest {
     }
   }
 
-  static TestInput[] validJson = new TestInput[] {
+  static Stream<TestInput> validJsonStream() {
+    return Stream.of(new TestInput[] {
       new TestInput("{}"),
       new TestInput("{\"key\":\"value\"}"),
       new TestInput("{\"outerKey\":{\"innerKey\":\"innerValue\"}}"),
       new TestInput("{\"string\":\"text\",\"number\":123,\"boolean\":true,\"nullValue\":null,\"object\":{\"nestedKey\":\"nestedValue\"},\"array\":[1,2,3]}"),
       new TestInput("{\"users\":[{\"id\":1,\"name\":\"Alice\",\"roles\":[\"admin\",\"user\"]},{\"id\":2,\"name\":\"Bob\",\"roles\":[\"user\"],\"profile\":{\"age\":30,\"interests\":[\"reading\",\"gaming\"]}}],\"settings\":{\"theme\":\"dark\",\"notifications\":{\"email\":true,\"sms\":false}}}"),
-  };
-
-  static Stream<TestInput> validJsonStream() {
-    return Stream.of(validJson);
+    });
   }
 
-  static TestInput[] invalidJson = new TestInput[] {
+  static Stream<TestInput> invalidJsonStream() {
+    return Stream.of(new TestInput[] {
       new TestInput("", "A JSONObject text must begin with '{' at 0 [character 1 line 1]"),
       new TestInput("{", "A JSONObject text must end with '}' at 1 [character 2 line 1]"),
       new TestInput("}", "A JSONObject text must begin with '{' at 1 [character 2 line 1]"),
@@ -57,10 +56,7 @@ public class ScriptEngineUtilityTest {
       new TestInput("true", "A JSONObject text must begin with '{' at 1 [character 2 line 1]"),
       new TestInput("false", "A JSONObject text must begin with '{' at 1 [character 2 line 1]"),
       new TestInput("10", "A JSONObject text must begin with '{' at 1 [character 2 line 1]"),
-  };
-
-  static Stream<TestInput> invalidJsonStream() {
-    return Stream.of(invalidJson);
+    });
   }
 
   @ParameterizedTest
