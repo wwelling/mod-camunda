174c88327dc0fa7753d95712c01104841fb92d4f
Kevin Day <kday@tamu.edu>
2024-06-13 15:42:29 -0500

    Show shared code base with additional testing and mocking.

--- src/test/java/org/folio/rest/camunda/service/BpmnModelFactoryTest.java
+++ src/test/java/org/folio/rest/camunda/service/BpmnModelFactoryTest.java
@@ -2,30 +2,31 @@ package org.folio.rest.camunda.service;
 
 import static org.folio.spring.test.mock.MockMvcConstant.UUID;
 import static org.folio.spring.test.mock.MockMvcConstant.VALUE;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.builder.AbstractFlowNodeBuilder;
 import org.camunda.bpm.model.bpmn.builder.ProcessBuilder;
-import org.camunda.bpm.model.bpmn.instance.BpmnModelElementInstance;
-import org.camunda.bpm.model.bpmn.instance.Definitions;
+import org.camunda.bpm.model.bpmn.builder.StartEventBuilder;
 import org.camunda.bpm.model.bpmn.instance.ExtensionElements;
-import org.camunda.bpm.model.xml.Model;
-import org.camunda.bpm.model.xml.instance.DomDocument;
+import org.camunda.bpm.model.bpmn.instance.Process;
+import org.camunda.bpm.model.bpmn.instance.camunda.CamundaField;
 import org.camunda.bpm.model.xml.instance.ModelElementInstance;
-import org.camunda.bpm.model.xml.type.ModelElementType;
-import org.camunda.bpm.model.xml.validation.ModelElementValidator;
-import org.camunda.bpm.model.xml.validation.ValidationResults;
 import org.folio.rest.camunda.exception.ScriptTaskDeserializeCodeFailure;
+import org.folio.rest.workflow.model.Node;
+import org.folio.rest.workflow.model.Setup;
 import org.folio.rest.workflow.model.Workflow;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -39,14 +40,15 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.camunda.bpm.model.bpmn.instance.Process;
-import org.camunda.bpm.model.bpmn.instance.camunda.CamundaField;
 
+@SuppressWarnings("deprecation")
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @ExtendWith(MockitoExtension.class)
 class BpmnModelFactoryTest {
+
+  @Mock
+  private AbstractFlowNodeBuilder<?, ?> abstractFlowNodeBuilder;
 
   @Mock
   private BpmnModelInstance bpmnModelInstance;
@@ -63,6 +65,12 @@ class BpmnModelFactoryTest {
   @Mock
   private Process process;
 
+  @Mock
+  private ProcessBuilder processBuilderMocked;
+
+  @Mock
+  private StartEventBuilder startEventBuilder;
+
   @SpyBean
   private ObjectMapper objectMapper;
 
@@ -71,45 +79,104 @@ class BpmnModelFactoryTest {
 
   private ProcessBuilder processBuilder;
 
+  private Node node;
+
+  private List<Node> nodes;
+
+  private Setup setup;
+
   private Workflow workflow;
 
   @BeforeEach
   void beforeEach() {
+    node = new MyNode();
+    nodes = new ArrayList<>();
+    nodes.add(node);
+
+    setup = new Setup();
+
     workflow = new Workflow();
     workflow.setId(UUID);
+    workflow.setSetup(setup);
 
     processBuilder = new ProcessBuilder(bpmnModelInstance, process);
   }
 
-  @SuppressWarnings("deprecation")
   @Test
-  void testFromWorkflowExceptionWorks() throws JsonProcessingException, ScriptTaskDeserializeCodeFailure {
+  void testFromWorkflowException() throws JsonProcessingException, ScriptTaskDeserializeCodeFailure {
     try (MockedStatic<Bpmn> utility = Mockito.mockStatic(Bpmn.class)) {
-      utility.when(() -> Bpmn.createExecutableProcess()).thenReturn(processBuilder);
+      commonUnmockedProcessBuilder(utility);
+      commonMockingsBasic();
 
-      doNothing().when(process).setCamundaHistoryTimeToLive(anyInt());
-      when(bpmnModelInstance.newInstance(ArgumentMatchers.<Class<ModelElementInstance>>any())).thenReturn(extensionElements, camundaField);
       when(objectMapper.writeValueAsString(any())).thenThrow(new MyException(VALUE));
-      doNothing().when(extensionElements).addChildElement(any());
-      when(bpmnModelInstance.getModelElementById(anyString())).thenReturn(modelElementInstance);
 
       bpmnModelFactory.fromWorkflow(workflow);
     }
   }
 
-  @SuppressWarnings("deprecation")
   @Test
-  void testFromWorkflowNoNodesWorks() throws JsonProcessingException, ScriptTaskDeserializeCodeFailure {
+  void testFromWorkflowNoNodesWorks() throws ScriptTaskDeserializeCodeFailure {
     try (MockedStatic<Bpmn> utility = Mockito.mockStatic(Bpmn.class)) {
-      utility.when(() -> Bpmn.createExecutableProcess()).thenReturn(processBuilder);
-
-      doNothing().when(process).setCamundaHistoryTimeToLive(anyInt());
-      when(bpmnModelInstance.newInstance(ArgumentMatchers.<Class<ModelElementInstance>>any())).thenReturn(extensionElements, camundaField);
-      doNothing().when(extensionElements).addChildElement(any());
-      when(bpmnModelInstance.getModelElementById(anyString())).thenReturn(modelElementInstance);
+      commonUnmockedProcessBuilder(utility);
+      commonMockingsBasic();
 
       bpmnModelFactory.fromWorkflow(workflow);
     }
+  }
+
+  @Test
+  void testFromWorkflowProcessBuilderGenericNodeThrowsException() throws JsonProcessingException, ScriptTaskDeserializeCodeFailure {
+    workflow.setNodes(nodes);
+
+    try (MockedStatic<Bpmn> utility = Mockito.mockStatic(Bpmn.class)) {
+      commonMockedProcessBuilder(utility);
+
+      when(processBuilderMocked.startEvent()).thenReturn(startEventBuilder);
+
+      RuntimeException exception = Assertions.assertThrows(RuntimeException.class, () -> {
+        bpmnModelFactory.fromWorkflow(workflow);
+      });
+
+      assertNotNull(exception);
+      assertTrue(exception.getMessage().contains("Workflow must start with a start event"));
+    }
+  }
+
+  /**
+   * A helper function for reducing repeated mock code between test functions.
+   *
+   * This function uses the instantiated and unmocked ProcessBuilder.
+   *
+   * @param utility The mocked static bmp class utility instance.
+   */
+  private void commonUnmockedProcessBuilder(MockedStatic<Bpmn> utility) {
+    utility.when(() -> Bpmn.createExecutableProcess()).thenReturn(processBuilder);
+  }
+
+  /**
+   * A helper function for reducing repeated mock code between test functions.
+   *
+   * This function uses the mocked ProcessBuilder.
+   *
+   * @param utility The mocked static bmp class utility instance.
+   */
+  private void commonMockedProcessBuilder(MockedStatic<Bpmn> utility) {
+    utility.when(() -> Bpmn.createExecutableProcess()).thenReturn(processBuilderMocked);
+
+    lenient().when(processBuilderMocked.getElement()).thenReturn(process);
+    lenient().when(processBuilderMocked.name(any())).thenReturn(processBuilderMocked);
+    lenient().when(processBuilderMocked.camundaHistoryTimeToLive(anyInt())).thenReturn(processBuilderMocked);
+    lenient().when(processBuilderMocked.camundaVersionTag(any())).thenReturn(processBuilderMocked);
+  }
+
+  /**
+   * A helper function for reducing repeated mock code between test functions.
+   */
+  private void commonMockingsBasic() {
+    doNothing().when(process).setCamundaHistoryTimeToLive(anyInt());
+    when(bpmnModelInstance.newInstance(ArgumentMatchers.<Class<ModelElementInstance>>any())).thenReturn(extensionElements, camundaField);
+    doNothing().when(extensionElements).addChildElement(any());
+    when(bpmnModelInstance.getModelElementById(anyString())).thenReturn(modelElementInstance);
   }
 
   /**
@@ -122,5 +189,11 @@ class BpmnModelFactoryTest {
     public MyException(String msg) {
       super(msg);
     }
+  }
+
+  /**
+   * Provide a non-abstract Node for easy instantiation.
+   */
+  private class MyNode extends Node {
   }
 }
