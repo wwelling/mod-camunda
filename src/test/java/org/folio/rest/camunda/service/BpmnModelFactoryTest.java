14413c0c3d9d59aec023a3b1ca390baf2e307c46
Kevin Day <kday@tamu.edu>
2024-06-13 14:30:11 -0500

    Show working exception related test.

--- src/test/java/org/folio/rest/camunda/service/BpmnModelFactoryTest.java
+++ src/test/java/org/folio/rest/camunda/service/BpmnModelFactoryTest.java
@@ -2,16 +2,23 @@ package org.folio.rest.camunda.service;
 
 import static org.folio.spring.test.mock.MockMvcConstant.UUID;
 import static org.folio.spring.test.mock.MockMvcConstant.VALUE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Collection;
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.bpm.model.bpmn.builder.ProcessBuilder;
+import org.camunda.bpm.model.bpmn.instance.BpmnModelElementInstance;
 import org.camunda.bpm.model.bpmn.instance.Definitions;
+import org.camunda.bpm.model.bpmn.instance.ExtensionElements;
 import org.camunda.bpm.model.xml.Model;
 import org.camunda.bpm.model.xml.instance.DomDocument;
 import org.camunda.bpm.model.xml.instance.ModelElementInstance;
@@ -20,9 +27,11 @@ import org.camunda.bpm.model.xml.validation.ModelElementValidator;
 import org.camunda.bpm.model.xml.validation.ValidationResults;
 import org.folio.rest.camunda.exception.ScriptTaskDeserializeCodeFailure;
 import org.folio.rest.workflow.model.Workflow;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
@@ -30,8 +39,10 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.camunda.bpm.model.bpmn.instance.Process;
+import org.camunda.bpm.model.bpmn.instance.camunda.CamundaField;
 
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @ExtendWith(MockitoExtension.class)
@@ -39,6 +50,15 @@ class BpmnModelFactoryTest {
 
   @Mock
   private BpmnModelInstance bpmnModelInstance;
+
+  @Mock
+  private CamundaField camundaField;
+
+  @Mock
+  private ExtensionElements extensionElements;
+
+  @Mock
+  private ModelElementInstance modelElementInstance;
 
   @Mock
   private Process process;
@@ -63,13 +83,29 @@ class BpmnModelFactoryTest {
 
   @SuppressWarnings("deprecation")
   @Test
-  void testFromWorkflowWorks() throws ScriptTaskDeserializeCodeFailure {
+  void testFromWorkflowExceptionWorks() throws JsonProcessingException, ScriptTaskDeserializeCodeFailure {
     try (MockedStatic<Bpmn> utility = Mockito.mockStatic(Bpmn.class)) {
       utility.when(() -> Bpmn.createExecutableProcess()).thenReturn(processBuilder);
 
       doNothing().when(process).setCamundaHistoryTimeToLive(anyInt());
+      when(bpmnModelInstance.newInstance(ArgumentMatchers.<Class<ModelElementInstance>>any())).thenReturn(extensionElements, camundaField);
+      when(objectMapper.writeValueAsString(any())).thenThrow(new MyException(VALUE));
+      doNothing().when(extensionElements).addChildElement(any());
+      when(bpmnModelInstance.getModelElementById(anyString())).thenReturn(modelElementInstance);
 
       bpmnModelFactory.fromWorkflow(workflow);
+    }
+  }
+
+  /**
+   * Provide an exception that exposes the string initializer for easy usage.
+   */
+  private class MyException extends JsonProcessingException {
+
+    private static final long serialVersionUID = -6261961424503639802L;
+
+    public MyException(String msg) {
+      super(msg);
     }
   }
 }
