a27aad39800c60a017b77cdad15cde94baf64b1d
William Welling <wwelling@tamu.edu>
2024-06-14 11:14:35 -0500

    Add more JavaDocs

--- src/test/java/org/folio/rest/camunda/utility/Parameters.java
+++ src/test/java/org/folio/rest/camunda/utility/Parameters.java
@@ -22,10 +22,42 @@ class Parameters<I, O> {
     this.exception = exception;
   }
 
+  /**
+   * Return typed Parameters when input and output are expected with no
+   * exceptions.
+   *
+   * @param <I>      generic input
+   * @param <O>      generic output
+   * @param input    provided to use when calling method to test
+   * @param expected output to make assertions against
+   * @return
+   */
   public static <I, O> Parameters<I, O> of(I input, O expected) {
     return new Parameters<I, O>(input, expected);
   }
 
+  /**
+   * Return typed Parameters when input and output are expected with no
+   * exceptions.
+   *
+   * @param <I>      generic input
+   * @param <O>      generic output
+   * @param input    provided to use when calling method to test
+   * @param expected output to make assertions against
+   * @return typed Parameter
+   */
+
+  /**
+   * Return typed Parameters when input and output are expected with expected
+   * exceptions. The output can be used here to assert exception message.
+   *
+   * @param <I>       generic input
+   * @param <O>       generic output
+   * @param input     provided to use when calling method to test
+   * @param expected  output to make assertions against
+   * @param exception an exception that expected to be thrown for input
+   * @return typed Parameter
+   */
   public static <I, O> Parameters<I, O> of(I input, O expected, Exception exception) {
     return new Parameters<I, O>(input, expected, exception);
   }
