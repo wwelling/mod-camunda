dd2b7db7f3204b8b8a081cec22c71bc0ade29551
William Welling <wwelling@tamu.edu>
2024-06-17 09:31:41 -0500

    Coverage for current setup delegate use cases
    The setup delegate is prepended to every workflow. The initial context does not work and has no use with workflows yet. The processors are also not currently used but may be required for manually registration and execution of scripts with the processor delegate.

---
+++ src/test/java/org/folio/rest/camunda/delegate/SetupDelegateTest.java
@@ -0,0 +1,128 @@
+package org.folio.rest.camunda.delegate;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.Expression;
+import org.camunda.bpm.model.bpmn.instance.FlowElement;
+import org.folio.rest.camunda.service.ScriptEngineService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+class SetupDelegateTest {
+
+  @Spy
+  protected ObjectMapper objectMapper;
+
+  @Spy
+  protected RuntimeService runtimeService;
+
+  @Spy
+  private ScriptEngineService scriptEngineService;
+
+  @Mock
+  Expression initialContext;
+
+  @Mock
+  Expression processors;
+
+  @Mock
+  DelegateExecution execution;
+
+  @Mock
+  FlowElement element;
+
+  @InjectMocks
+  SetupDelegate delegate;
+
+  @BeforeEach
+  void beforeEach() {
+    // this is unique per delegate
+    delegate.setInitialContext(initialContext);
+    delegate.setProcessors(processors);
+  }
+
+  @ParameterizedTest
+  @MethodSource("executionStream")
+  @SuppressWarnings("unchecked")
+  void testExecute(String name, String initialContextValue, String processorsValue, Class<Exception> exception) throws Exception {
+    if (Objects.nonNull(exception)) {
+      assertThrows(exception, () -> delegate.execute(execution));
+    } else {
+
+      when(execution.getTenantId()).thenReturn("diku");
+      when(execution.getBpmnModelElementInstance()).thenReturn(element);
+      when(element.getName()).thenReturn(name);
+      when(initialContext.getValue(any(DelegateExecution.class))).thenReturn(initialContextValue);
+      when(processors.getValue(any(DelegateExecution.class))).thenReturn(processorsValue);
+
+      delegate.execute(execution);
+
+      verify(element, times(1)).getName();
+      verify(initialContext, times(1)).getValue(any(DelegateExecution.class));
+      verify(processors, times(1)).getValue(any(DelegateExecution.class));
+      verify(objectMapper, times(1)).readValue(eq(initialContextValue), any(TypeReference.class));
+
+      // initialContext are not yet used and are subject to removal
+      // for each initial context variable
+      // verify objectMapper writeValueAsString and execution setVariable for each initial context
+
+      verify(execution, times(1)).setVariable(eq("timestamp"), anyString());
+      verify(execution, times(1)).setVariable("tenantId", "diku");
+
+      // processors are not yet used and are subject to removal
+      // for each processor
+      // mock processor getScriptType getExtension chain
+      // mock processor getFunctionName and getCode
+      // mock scriptEngineService registerScript
+      // verify objectMapper writeValueAsString and execution setVariable for each initial context
+    }
+  }
+
+  /**
+   * Helper function for parameterized test providing tests with
+   *
+   * @return
+   *   The arguments array stream with the stream columns as:
+   *     - delegate name
+   *     - initial context variables (JSON map of type Map<String, Object>)
+   *     - processors to register (JSON list of type EmbeddedProcessor)
+   */
+  private static Stream<Arguments> executionStream() {
+    return Stream.of(
+      Arguments.of("SetupDelegate", null, null, NullPointerException.class),
+      Arguments.of("SetupDelegate", null, "",   NullPointerException.class),
+      Arguments.of("SetupDelegate", null, "[]", NullPointerException.class),
+      Arguments.of("SetupDelegate", "",   null, NullPointerException.class),
+      Arguments.of("SetupDelegate", "",   "",   NullPointerException.class),
+      Arguments.of("SetupDelegate", "",   "[]", NullPointerException.class),
+      Arguments.of("SetupDelegate", "{}", null, NullPointerException.class),
+      Arguments.of("SetupDelegate", "{}", "",   NullPointerException.class),
+      Arguments.of("SetupDelegate", "{}", "[]", null)
+    );
+  }
+
+
+}
